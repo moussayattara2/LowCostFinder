316ff82be07d8152c274f86b706659b0fcd9d4a5
Moussa <moussayattara2@yahoo.fr>
2016-07-24 14:59:19 +0200

    Add DAO Client

---
+++ src/com/lowcostfinder/java/DAO/DAOClient.java
@@ -0,0 +1,18 @@
+package com.lowcostfinder.java.DAO;
+
+public class DAOClient {
+
+	protected void insertClient(){
+		
+	}
+	protected void updateClient(){
+		
+	}
+	
+	protected void deleteClient(){
+		
+	}
+	protected void getClient(){
+		
+	}
+}
