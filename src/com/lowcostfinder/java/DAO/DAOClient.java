c03d15db316217bc5abb5dbc3bfeec9f795a51f4
Moussa <moussayattara2@yahoo.fr>
2016-07-27 03:08:34 +0200

    Update DAOClient with IEDAOClient

---
+++ src/com/lowcostfinder/java/DAO/DAOClient.java
@@ -0,0 +1,51 @@
+package com.lowcostfinder.java.DAO;
+
+import java.util.List;
+import java.util.List.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.lowcostfinder.java.domain.Client;
+import com.lowcostfinder.java.DAO.IEDAOClient;
+
+public class DAOClient implements IEDAOClient {
+
+	@Override
+	public void insertClient(Client client) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateClient(Client client) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteClient(Client client) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getClient() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Client getClientbyId(int idclient) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Client> getAllClient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
